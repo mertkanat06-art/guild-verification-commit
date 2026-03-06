ba85db1a9c93056ea8a37a8086c5ded2e46341f8
mertkanat06-art <mertkanat06@gmail.com>
2026-03-06 18:05:30 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T15:05:32.458Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-06T15:05:34.075Z
+Commit: Fix broken links in documentation
