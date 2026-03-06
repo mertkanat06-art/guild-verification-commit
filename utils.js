aea3f1c4defdc3e46ee9ec5209a6cf545b03ffb2
mertkanat06-art <mertkanat06@gmail.com>
2026-03-06 18:05:28 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T15:05:28.596Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-06T15:05:32.458Z
+Commit: Fix memory leak in event handlers
