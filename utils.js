bbe91f840a45a658e36c8b76fab0ca4f1b2a44f4
mertkanat06-art <mertkanat06@gmail.com>
2026-03-06 18:04:23 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T15:04:24.162Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-06T15:04:27.130Z
+Commit: Patch API endpoint error handling
