b2ddd22279adc13d47f5600de73c4bd7643d9b7a
mertkanat06-art <mertkanat06@gmail.com>
2026-03-06 18:05:44 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T15:05:42.472Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-06T15:05:47.850Z
+Commit: Resolve null pointer exception in user service
